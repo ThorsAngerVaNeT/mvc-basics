e7859c7690d6121de4f9a88a7e4091a4710c2360
Vadzim Antonau <vanet@tut.by>
2022-08-12 08:48:39 +0300

    fix: module import, add type to class

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,24 +1,25 @@
-import { baseUrl } from "../../data"
+import { baseUrl } from '../../data.js';
 
 export class Controller {
   constructor(model, view, type) {
-    this.model = model
-    this.view = view
+    this.model = model;
+    this.view = view;
+    this.type = type;
   }
 
   start() {
-    this.view.drawSkeleton()
-    this.view.linkListeners(this.model.isLoading, this.model.data)
-    this.getAllData()
+    this.view.drawSkeleton();
+    this.view.linkListeners(this.model.isLoading, this.model.data);
+    this.getAllData();
   }
 
   getAllData() {
-    console.log("getAllData")
+    console.log('getAllData');
     this.model.getUsers({
-      method: "GET",
-      url: `${baseUrl}/${type}`,
+      method: 'GET',
+      url: `${baseUrl}/${this.type}`,
       element: this.view.usersContainer,
       event: this.view.event,
-    })
+    });
   }
 }
